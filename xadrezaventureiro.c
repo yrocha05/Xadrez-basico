bdcc9f881981ba32349c4ab01321a209256b8b43
Yuri Rocha <130370083+yrocha05@users.noreply.github.com>
2025-05-21 18:50:30 -0300

    Nivel Aventureiro

---
+++ xadrezaventureiro.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+int main (){
+    int cavalo;
+    int torre1;
+    int bispo1 = 1;
+    int rainha = 1;
+
+    printf("Movimento Torre!\n");
+    for (int torre1 = 0; torre1 <= 4; torre1++)
+    {
+       printf("Direita\n");
+    }
+    
+        printf("\n");
+
+    printf("Movimento Bispo!\n");
+
+    while (bispo1 <= 5){
+       printf("Cima,Direita\n");
+       bispo1++;
+    }
+
+        
+        printf("\n");
+
+    printf("Movimento Rainha!\n");
+
+    do
+    {
+       printf("Esquerda\n");
+       rainha++;
+    } while (rainha <= 8);
+    
+        printf("\n");
+
+    for ( cavalo = 1; cavalo <= 1; cavalo++)
+    printf("Movimento Cavalo!\n");
+    {
+        for (int l = 1; l <= 2; l++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
+    
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
